1db2e6fe5c83e408f921fd860b314753a7b133dd
imreklest <harefa19gilbert@gmail.com>
2023-07-13 23:03:23 +0700

    nambah migrasi product tipis tipis

--- src/database/migrations/20230711151605-create_products_table.js
+++ src/database/migrations/20230711151605-create_products_table.js
@@ -3,7 +3,22 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-   
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      id_outlets: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      timestamp: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    }
   },
 
   async down (queryInterface, Sequelize) {
