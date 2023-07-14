d7c47590c08b395fc7a63435419b5b543d2a7b24
marcelino0707 <marcelinoahong0707@gmail.com>
2023-07-14 23:11:05 +0700

    template

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,9 @@ route.get('/outlet', outlet.getOutlets);
 
 // Product
 route.get('/product', product.getProducts);
+route.post('/product', product.createProduct);
+route.get('/product/:id', product.getProductById);
+route.patch('/product/:id', product.updateProduct);
+route.delete('/product/:id', product.deleteProduct);
 
 module.exports = route;
