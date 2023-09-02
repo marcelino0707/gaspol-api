f410ab5dc67036e0676e2685a59af0d6c267f474
marcelino0707 <marcelinoahong0707@gmail.com>
2023-09-02 22:48:07 +0700

    coba2 edit app,js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,34 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer();
 
-const app = express()
+const app = express();
 dotenv.config();
 
-// Parse requests
-app.use(bodyParser.json())
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing application/xwww-form-urlencoded
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// for parsing multipart/form-data
+app.use(upload.array());
 
 // Cors
 app.use(cors());
 
 // Routes
-app.use(require('./routes'))
+app.use(require("./routes"));
 
 // Start server
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
