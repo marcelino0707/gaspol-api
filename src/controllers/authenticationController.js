598a3b0a169c226f787157912f00d6eda0221abf
marcelino0707 <marcelinoahong0707@gmail.com>
2023-10-05 08:25:15 +0700

    tambah outlet name di auth

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,12 +1,14 @@
-const User = require("../models/user");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+const Outlet = require("../models/outlet");
 
 exports.login = async (req, res) => {
   const {username, password} = req.body
   try {
     if(username && password) {
         const user = await User.getByUsername(username);
+
         if(!user) {
             return res.status(401).json({
                 message: "Username tidak terdaftar!"
@@ -16,12 +18,19 @@ exports.login = async (req, res) => {
         const match = await bcrypt.compare(password, user.password);
 
         if(match) {
+            let outletName = "GASPOL";
+            if(!user.role === 1) {
+                const outlet = await Outlet.getByOutletId(user.outlet_id);
+                outletName = outlet.name;
+            } 
+
             const token = jwt.sign(
                 {
                     userId: user.id,
                     username: user.username,
                     role: user.role,
                     outlet_id: user.outlet_id,
+                    outlet_name: outletName,
                     menu_access: user.menu_access,
                 },
                 process.env.JWT_SECRET_KEY,
