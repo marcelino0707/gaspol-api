538cc9ca598ea45d0f51845f8e960ab6d5a10a0c
marcelino0707 <marcelinoahong0707@gmail.com>
2023-12-01 14:03:18 +0700

    show is refunded too in get all transaction success

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -32,7 +32,7 @@ const Transaction = {
               outlet_id +
               " AND DATE(transactions.updated_at) = '" +
               date +
-              "' AND transactions.invoice_due_date IS NOT NULL AND transactions.is_refunded = 0 AND transactions.deleted_at IS NULL",
+              "' AND transactions.invoice_due_date IS NOT NULL AND transactions.deleted_at IS NULL",
             (error, results) => {
               disconnectDB(connection);
               if (error) {
