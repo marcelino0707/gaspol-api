34cdb614a235efc2168bfd563bd1cf48e699f16a
marcelino0707 <marcelinoahong0707@gmail.com>
2025-03-07 22:06:51 +0700

    complaint feature v1.1

--- src/models/complaint.js
+++ src/models/complaint.js
@@ -5,7 +5,7 @@ const Complaint = {
     return new Promise((resolve, reject) => {
       connectDB()
         .then((connection) => {
-          connection.query('SELECT name, title, message, sent_at, log_last_outlet, cache_transaction, cache_failed_transaction, cache_success_transaction, cache_history_transaction FROM outlet_complaints WHERE outlet_id = ?', outlet_id, (error, results) => {
+          connection.query('SELECT name, title, message, sent_at, log_last_outlet, cache_transaction, cache_failed_transaction, cache_success_transaction, cache_history_transaction FROM outlet_complaints WHERE outlet_id = ? ORDER BY sent_at DESC LIMIT 20', outlet_id, (error, results) => {
             disconnectDB(connection);
             if (error) {
               reject(error);
