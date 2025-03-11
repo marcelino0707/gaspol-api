0db06cf8ab09fbfc24c3bf4d25dc3320cd017e2b
marcelino0707 <marcelinoahong0707@gmail.com>
2025-03-12 01:42:39 +0700

    set cors for cms

--- src/app.js
+++ src/app.js
@@ -12,7 +12,9 @@ app.use(bodyParser.json({ limit: '20mb' })); // Set to 10mb or any size you need
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: true })); // Set to 10mb or any size you need
 
 // Enable CORS
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
 
 // Serving static files from the "public" directory
 app.use("/public", express.static("public"));
