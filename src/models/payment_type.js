23a3506abc6c5e28f78570460611407c634e5e92
bayufrd <55101246+bayufrd@users.noreply.github.com>
2025-05-16 18:18:06 +0700

    Done CMS urut Sesuai order

--- src/models/payment_type.js
+++ src/models/payment_type.js
@@ -23,7 +23,8 @@ const PaymentType = {
   getAllCMS: (outletId) => {
     return new Promise((resolve, reject) => {
       connectDB().then((connection) => {
-        connection.query("SELECT payment_types.id, payment_types.name, payment_types.payment_category_id AS payment_category_id, payment_categories.name AS payment_category_name, payment_types.is_active FROM payment_types JOIN payment_categories ON payment_types.payment_category_id = payment_categories.id WHERE payment_types.outlet_id = ? AND payment_types.deleted_at IS NULL", outletId, (error, results) => {
+        // connection.query("SELECT payment_types.id, payment_types.name, payment_types.payment_category_id AS payment_category_id, payment_categories.name AS payment_category_name, payment_types.is_active, payment_types.order FROM payment_types JOIN payment_categories ON payment_types.payment_category_id = payment_categories.id WHERE payment_types.outlet_id = ? AND payment_types.deleted_at IS NULL", outletId, (error, results) => {
+        connection.query("SELECT payment_types.id, payment_types.name, payment_types.payment_category_id AS payment_category_id, payment_categories.name AS payment_category_name, payment_types.is_active, payment_types.order FROM payment_types JOIN payment_categories ON payment_types.payment_category_id = payment_categories.id WHERE payment_types.outlet_id = ? AND payment_types.deleted_at IS NULL ORDER BY payment_types.order ASC", outletId, (error, results) => {
           disconnectDB(connection);
           if (error) {
             reject(error);
